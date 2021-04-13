b7e0f54355074919dd114ab2f7100b3f2a614842
maryamboneh <maryamboneh@gmail.com>
2021-04-14 00:25:54 +0430

    fix . multiply matrixs

--- Assignment6/matrix.py
+++ Assignment6/matrix.py
@@ -5,24 +5,15 @@ n = int(input('n = '))
 m = int(input('m = '))
 k = int(input('k = '))
 
-a =  [[1 for i in range(m)] for j in range(n)]
-b = [[1 for i in range(k)] for j in range(m)]
+a = [[random.randint(1, 9) for i in range(m)] for j in range(n)]
+b = [[random.randint(1, 9) for i in range(k)] for j in range(m)]
 result = [[0 for i in range(k)] for j in range(n)]
 
 for i in range(n):
     for j in range(m):
-        a[i][j] = random.randint(1,9)
-        
-for i in range(m):
-    for j in range(k):
-        b[i][j] = random.randint(1,9)
-       
-for i in range(n):
-    for j in range(k):
-        for k in range(m):
-            result[i][j] += (a[i][k] * b [k][j]) 
-        
+        for p in range(k):
+            result[i][p] += (a[i][j] * b[j][p])
+
 print('a : ', a)
 print('b : ', b)
 print('result : ', result)
-
