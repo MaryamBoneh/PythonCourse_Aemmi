18e8874e0b8b5aae778b3adb0cc500c9b46c4c99
maryamboneh <maryamboneh@gmail.com>
2021-06-06 18:09:29 +0430

    timer - not complete

--- Assignment19/Alarms and Clock/main.py
+++ Assignment19/Alarms and Clock/main.py
@@ -2,13 +2,90 @@
 import sys
 import os
 
-from PySide6.QtWidgets import QApplication, QLabel, QWidget
+from PySide6.QtWidgets import QApplication, QGridLayout, QLabel, QProgressBar, QPushButton, QWidget
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtCore import QThread, Qt
+from PySide6.QtCore import QThread, Qt, QLine
+from PySide6.QtGui import QIcon
+from functools import partial
 import time
 
 
+class Alarm(QThread):
+    def __init__(self):
+        QThread.__init__(self)
+        pass
+
+
 class Timer(QThread):
+    duration = []
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.h = 0
+        self.m = 0
+        self.s = 0
+
+    def add(self):
+        gLaout = QGridLayout()
+        self.label_name = QLabel()
+        self.label_time = QLabel()
+        # self.progressbar = QProgressBar()
+        btn_del = QPushButton()
+        btn_start = QPushButton()
+        self.label_name.setText(widget.ui.txt_name_timer.text())
+        self.label_time.setText(widget.ui.txt_value_timer.text())
+        btn_del.setText('×')
+        # self.progressbar.setValue(0)
+        
+        btn_start.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/play.png'))
+        btn_del.setStyleSheet('min-width: 30px; max-width: 30px; font-size: 18px; min-height: 30px; border-radius: 15px; background-color: #000; color: #fff; margin-bottom: 15px;')
+        btn_start.setStyleSheet('min-width: 30px; max-width: 30px; min-height: 30px; border-radius: 15px; background-color: #000; color: #fff; margin-bottom: 15px;')
+        self.label_time.setStyleSheet('font: "Seven Segment"; font-size: 16px; padding-left: 20px; margin-bottom: 15px;')
+        self.label_name.setStyleSheet('font: "Assignment19/Alarms and Clock/assets/font/Seven Segment"; font-size: 16px; padding-left: 5px; margin-bottom: 15px;')
+
+        widget.ui.vl_timers.addLayout(gLaout, widget.length_timr)
+        gLaout.addWidget(self.label_name, 0, 0, alignment=Qt.Alignment())
+        gLaout.addWidget(self.label_time, 0, 1, alignment=Qt.Alignment())
+        gLaout.addWidget(btn_del, 0, 2, alignment=Qt.Alignment())
+        # gLaout.addWidget(self.progressbar, 2, 0, alignment=Qt.Alignment())
+        gLaout.addWidget(btn_start, 0, 3, alignment=Qt.Alignment())
+
+        btn_start.clicked.connect(Timer.pause)
+        widget.length_timr += 1
+        Timer.duration = self.label_time.text().split(':')
+        self.h = int(Timer.duration[0])
+        self.m = int(Timer.duration[1])
+        self.s = int(Timer.duration[2])
+        
+        # self.total_sec = self.passed_sec = (self.h*3600 + self.m*60 + self.s)
+
+    def decrease(self):
+        # self.passed_sec -= 1
+        if self.h == 0 and self.m == 0 and self.s == 0:
+            return
+
+        if self.s == 0 and self.m > 0:
+            self.s = 59
+            self.m -= 1
+
+        if self.m == 0 and self.h > 0:
+            self.m = 59
+            self.h -= 1
+        self.s -= 1
+
+    def run(self):
+        while True:
+            self.decrease()
+            # self.percent = (self.total_sec - self.passed_sec) * 100 / self.total_sec
+            # self.progressbar.setValue(int(self.percent))
+            self.label_time.setText(f"{self.h}:{self.m}:{self.s}")
+            time.sleep(1)
+
+    def pause(self):
+        print('in pause')
+
+
+class Stopwatch(QThread):
     def __init__(self):
         QThread.__init__(self)
         self.h = 0
@@ -46,47 +123,44 @@ class Main(QWidget):
         self.ui.btn_stopwatch_start.clicked.connect(self.startStopWatch)
         self.ui.btn_stopwatch_save.clicked.connect(self.saveStopWatch)
         self.ui.btn_stopwatch_stop.clicked.connect(self.stopStopWatch)
+        self.ui.btn_add_timer.clicked.connect(self.addTimer)
+        self.ui.btn_add_timer.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/add.png'))
 
-        self.timer = Timer()
+        self.stopwatch = Stopwatch()
         self.ui.show()
-        self.length = 0
+        self.length_stw = 0
+        self.length_timr = 0
+
+# -------------------------------------STOP WATCH----------------------------------------
 
     def pauseStopWatch(self):
-        self.timer.terminate()
+        self.stopwatch.terminate()
 
     def stopStopWatch(self):
-        self.timer.terminate()
-        self.timer.reset()
+        self.stopwatch.terminate()
+        self.stopwatch.reset()
         self.ui.lbl_stopwatch.setText("0:0:0")
 
     def startStopWatch(self):
-        self.timer.start()
+        self.stopwatch.start()
 
     def saveStopWatch(self):
-        self.length += 1
-        # last_saved = self.ui.lbl_stopwatch.text().split(':')
-        
-        # h = Timer.h - int(last_saved[0])
-        # m = Timer.m - int(last_saved[1])
-        # s = Timer.s - int(last_saved[2])
-        
-        # if s < 0:
-        #     s += 60
-        #     m -= 1
-        # if m < 0:
-        #     m += 60
-        #     h -= 1
-        
+        self.length_stw += 1
+
         label_num = QLabel()
-        # label_time = QLabel()
         label_total = QLabel()
-        label_num.setText(str(self.length))
-        # label_time.setText(f'{h}:{m}:{s}')
+        label_num.setText(str(self.length_stw))
         label_total.setText(self.ui.lbl_stopwatch.text())
         label_num.setStyleSheet('max-width: 50px; padding-left: 20px;')
-        self.ui.gl_saved_times.addWidget(label_num, self.length, 0, alignment=Qt.Alignment())
-        # self.ui.gl_saved_times.addWidget(label_time, self.length, 1, alignment=Qt.Alignment())
-        self.ui.gl_saved_times.addWidget(label_total, self.length, 1, alignment=Qt.Alignment())
+        self.ui.gl_saved_times.addWidget(label_num, self.length_stw, 0, alignment=Qt.Alignment())
+        self.ui.gl_saved_times.addWidget(label_total, self.length_stw, 1, alignment=Qt.Alignment())
+
+# ---------------------------------------- TIMER -----------------------------------------
+
+    def addTimer(self):
+        self.timer = Timer()
+        self.timer.add()
+        self.timer.start()
 
 
 if __name__ == "__main__":
