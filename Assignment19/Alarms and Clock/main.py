3c6c2e47c1fed2a5d920d9ac31f1f23be7112450
maryamboneh <maryamboneh@gmail.com>
2021-06-12 21:36:08 +0430

    alarm and clock - assignment 19

--- Assignment19/Alarms and Clock/main.py
+++ Assignment19/Alarms and Clock/main.py
@@ -5,11 +5,12 @@ import os
 from PySide6.QtWidgets import QApplication, QGridLayout, QLabel, QProgressBar, QPushButton, QWidget
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QThread, Qt, QLine
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import  QIcon
+from PySide6 import QtGui
+from win10toast import ToastNotifier
 from functools import partial
 from datetime import datetime
 import time
-from win10toast import ToastNotifier
 import winsound
 
 
@@ -20,17 +21,14 @@ class Alarm(QThread):
         self.m = widget.ui.spinBox_m.value()
         self.s = widget.ui.spinBox_s.value()
         
-        
     def check(self):    
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
         current_time = current_time.split(':')
-        #print(current_time , self.h, self.m, self.s)
         
         if self.h == int(current_time[0]) and self.m == int(current_time[1]) and self.s == int(current_time[2]):
             self.toast = ToastNotifier()
             self.toast.show_toast("Alarm Time", "barkhiiiiiz azizam", duration=20, icon_path="Assignment19/Alarms and Clock/assets/img/icon.png")
-            
 
     def run(self):
         while True:
@@ -48,49 +46,16 @@ class Timer(QThread):
         self.s = 0
 
     def add(self):
-        self.gLaout = QGridLayout()
-        self.label_name = QLabel()
-        self.label_time = QLabel()
-        # self.progressbar = QProgressBar()
-        self.btn_del = QPushButton()
-        self.btn_start = QPushButton()
-        self.btn_pause = QPushButton()
-        self.label_name.setText(widget.ui.txt_name_timer.text())
-        self.label_time.setText(widget.ui.txt_value_timer.text())
-        self.btn_del.setText('×')
-        # self.progressbar.setValue(0)
+        widget.ui.lbl_name_timer.setText(widget.ui.txt_name_timer.text())
+        widget.ui.lbl_time_timer.setText(widget.ui.txt_value_timer.text())
         
-        self.btn_start.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/play.png'))
-        self.btn_pause.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/pause.png'))
-        self.btn_del.setStyleSheet('min-width: 30px; max-width: 30px; font-size: 18px; min-height: 30px; border-radius: 15px; background-color: #000; color: #fff; margin-bottom: 15px;')
-        self.btn_start.setStyleSheet('min-width: 30px; max-width: 30px; min-height: 30px; border-radius: 15px; background-color: #000; color: #fff; margin-bottom: 15px;')
-        self.btn_pause.setStyleSheet('min-width: 30px; max-width: 30px; min-height: 30px; border-radius: 15px; background-color: #000; color: #fff; margin-bottom: 15px;')
-        self.label_time.setStyleSheet('font: "Seven Segment"; font-size: 16px; padding-left: 20px; margin-bottom: 15px;')
-        self.label_name.setStyleSheet('font: "Assignment19/Alarms and Clock/assets/font/Seven Segment"; font-size: 16px; padding-left: 5px; margin-bottom: 15px;')
-
-        widget.ui.vl_timers.addLayout(self.gLaout, widget.length_timr)
-        self.gLaout.addWidget(self.label_name, 0, 0, alignment=Qt.Alignment())
-        self.gLaout.addWidget(self.label_time, 0, 1, alignment=Qt.Alignment())
-        self.gLaout.addWidget(self.btn_del, 0, 2, alignment=Qt.Alignment())
-        # self.gLaout.addWidget(self.progressbar, 2, 0, alignment=Qt.Alignment())
-        self.gLaout.addWidget(self.btn_start, 0, 3, alignment=Qt.Alignment())
-        self.gLaout.addWidget(self.btn_pause, 0, 4, alignment=Qt.Alignment())
-
-        self.btn_start.clicked.connect(Timer.resume)
-        self.btn_pause.clicked.connect(Timer.pause)
-        self.btn_del.clicked.connect(Timer.stop)
-        
-        widget.length_timr += 1
-        Timer.duration = self.label_time.text().split(':')
+        Timer.duration = widget.ui.lbl_time_timer.text().split(':')
         self.h = int(Timer.duration[0])
         self.m = int(Timer.duration[1])
         self.s = int(Timer.duration[2])
-        
-        # self.total_sec = self.passed_sec = (self.h*3600 + self.m*60 + self.s)
 
         
     def decrease(self):
-        # self.passed_sec -= 1
         if self.h == 0 and self.m == 0 and self.s == 0:
             return
 
@@ -106,21 +71,8 @@ class Timer(QThread):
     def run(self):
         while True:
             self.decrease()
-            # self.percent = (self.total_sec - self.passed_sec) * 100 / self.total_sec
-            # self.progressbar.setValue(int(self.percent))
-            self.label_time.setText(f"{self.h}:{self.m}:{self.s}")
+            widget.ui.lbl_time_timer.setText(f"{self.h}:{self.m}:{self.s}")
             time.sleep(1)
-
-    def pause(self):
-        widget.pauseTimer()
-        
-    def resume(self):
-        widget.resumeTimer()
-            
-    def stop(self):
-        widget.pauseTimer()
-        self.btn_del.hide()
-        self.btn_pause.hide()
         
 
 class Stopwatch(QThread):
@@ -157,6 +109,9 @@ class Main(QWidget):
         super(Main, self).__init__()
         loader = QUiLoader()
         self.ui = loader.load('Assignment19/Alarms and Clock/form.ui')
+        self.ui.setWindowIcon(QtGui.QIcon('Assignment19/Alarms and Clock/assets/img/icon.png'))
+        self.ui.show()
+        
 
 #----------------------------------- STOP WATCH UI --------------------------------------
 
@@ -165,15 +120,21 @@ class Main(QWidget):
         self.ui.btn_stopwatch_stop.clicked.connect(self.stopStopWatch)
         self.ui.btn_add_timer.clicked.connect(self.addTimer)
         self.ui.btn_add_timer.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/add.png'))
+        self.ui.btn_stopwatch_start.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/play.png'))
+        self.ui.btn_stopwatch_stop.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/pause.png'))
+        self.ui.btn_stopwatch_save.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/flag.png'))
 
         self.stopwatch = Stopwatch()
-        self.ui.show()
         self.length_stw = 0
         
 #-------------------------------------- TIMER UI ----------------------------------------
-
-        self.length_timr = 0
-        self.timers_list = []
+       
+        self.ui.timer_btn_start.clicked.connect(self.resumeTimer)
+        self.ui.timer_btn_pause.clicked.connect(self.pauseTimer)
+        self.ui.timer_btn_del.clicked.connect(self.stopTimer)
+        self.ui.timer_btn_start.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/play.png'))
+        self.ui.timer_btn_pause.setIcon(QIcon('Assignment19/Alarms and Clock/assets/img/pause.png'))
+        self.ui.timer_btn_del.setText('×')
         
 #-------------------------------------- ALARM UI ----------------------------------------
 
@@ -207,7 +168,6 @@ class Main(QWidget):
 # ---------------------------------------- TIMER -----------------------------------------
 
     def addTimer(self):
-        # self.timers_list.append(Timer())
         self.timer = Timer()
         self.timer.add()
         self.timer.start()
@@ -218,14 +178,18 @@ class Main(QWidget):
     def resumeTimer(self):
         self.timer.start()        
         
+    def stopTimer(self):
+        self.timer.terminate()
+        self.ui.lbl_name_timer.setText("")
+        self.ui.lbl_time_timer.setText("0:0:0")
+        self.ui.txt_name_timer.setText("")
+        self.ui.txt_value_timer.setText("")
+        
 # ---------------------------------------- ALARM -----------------------------------------
 
     def setAlarm(self):
         self.alarm = Alarm()
         self.alarm.start()
-
-
-
 
 
        
