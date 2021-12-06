7f9c0d44be3cbed11db6d456f80f0dab0adfc0e0
maryamboneh <maryamboneh@gmail.com>
2021-12-06 10:41:44 +0330

    fix indents

--- snow/Snow.py
+++ snow/Snow.py
@@ -1,4 +1,4 @@
-import cv2, glob, random, argparse
+import cv2, glob, random
 from PIL import Image
 
 
@@ -16,33 +16,34 @@ class Snow:
 
 
 image = cv2.imread('snow/img/Snow.jpeg', cv2.IMREAD_COLOR)
+output_addres = 'snow/img/shots/'
 image_temp = image.copy()
 row, col, _ = image.shape
+
 snow_list = []
 c = 10
 
 for _ in range(450):
-  snow_list.append(Snow())
+    snow_list.append(Snow())
 
 for j in range(89):
-  for i, s in enumerate(snow_list):
-    s.move()
-  image = image_temp.copy()
-  for a in snow_list:
-    cv2.circle(image, (int(a.x), a.y), a.width, (240, 240, 240), -1)
-  c += 1
-  cv2.imwrite(f'snow/img/shots/{c}.png', image)
+    for i, s in enumerate(snow_list):
+        s.move()
+    image = image_temp.copy()
+    for a in snow_list:
+        cv2.circle(image, (int(a.x), a.y), a.width, (240, 240, 240), -1)
+    c += 1
+    cv2.imwrite(f'{output_addres}{c}.png', image)
 
-
-imgs = glob.glob(f'snow/img/shots/*.png')
+imgs = glob.glob(f'{output_addres}*.png')
 imgs.sort()
 frames = []
-print(imgs)
+
 for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
 
-frames[0].save(f'snow/img/shots/png_to_gif.gif', format='GIF',
+frames[0].save(f'{output_addres}png_to_gif.gif', format='GIF',
                append_images=frames[1:],
                save_all=True,
                duration=120, loop=0)
